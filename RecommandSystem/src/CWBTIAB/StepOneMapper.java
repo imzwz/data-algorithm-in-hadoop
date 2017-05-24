c9e9806de6b2cdbb34edc8b23d02c75d884e89ea
winn <iamzwz3@gmail.com>
2017-05-24 19:09:09 +0800

    update recommand system cwbtiab

--- RecommandSystem/src/CWBTIAB/StepOneMapper.java
+++ RecommandSystem/src/CWBTIAB/StepOneMapper.java
@@ -1,7 +1,17 @@
 package CWBTIAB;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
 /**
  * Created by winn on 17/5/23.
  */
-public class StepOneMapper {
+public class StepOneMapper extends Mapper<LongWritable, Text, Text, Text> {
+    @Override
+    public void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException{
+        
+    }
 }
