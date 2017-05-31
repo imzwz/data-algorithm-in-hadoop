b784cd4be9eba7532e26453fe716ba91e89706e2
winn <iamzwz3@gmail.com>
2017-05-31 20:51:43 +0800

    update secondary sort add spark file

---
+++ secondary_sort/src/second_spark/CustomPartitioner.java
@@ -0,0 +1,11 @@
+package second_spark;
+
+import org.apache.spark.Partitioner;
+import scala.Tuple2;
+
+/**
+ * Created by winn on 17/5/31.
+ */
+public class CustomPartitioner extends Partitioner {
+    
+}
