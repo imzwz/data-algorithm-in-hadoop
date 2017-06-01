993501f3a2188def2ff3bba5c9d4eaef3de26294
winn <iamzwz3@gmail.com>
2017-06-01 11:07:33 +0800

    update secondary spark

--- secondary_sort/src/second_spark/CustomPartitioner.java
+++ secondary_sort/src/second_spark/CustomPartitioner.java
@@ -1,5 +1,6 @@
 package second_spark;
 
+
 import org.apache.spark.Partitioner;
 import scala.Tuple2;
 
@@ -7,5 +8,55 @@ import scala.Tuple2;
  * Created by winn on 17/5/31.
  */
 public class CustomPartitioner extends Partitioner {
-    
+    private static final long serialVersionUID = -4130944499909424595L;
+
+    private final int numPartitions;
+    public CustomPartitioner(int partitions){
+        assert (partitions > 0);
+        this.numPartitions = partitions;
+    }
+
+    @Override
+    public int getPartition(Object key){
+        if(key == null){
+            return 0;
+        }else if(key instanceof Tuple2){
+            @SuppressWarnings("unchecked")
+            Tuple2<String, Integer> tuple2 = (Tuple2<String, Integer>) key;
+            return Math.abs(tuple2._1.hashCode() % numPartitions);
+        }else{
+            return Math.abs(key.hashCode() % numPartitions);
+        }
+    }
+
+    @Override
+    public int numPartitions(){
+        return numPartitions;
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + numPartitions;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj){
+            return true;
+        }
+        if(obj == null){
+            return false;
+        }
+        if(!(obj instanceof CustomPartitioner)){
+            return false;
+        }
+        CustomPartitioner other = (CustomPartitioner) obj;
+        if(numPartitions != other.numPartitions){
+            return false;
+        }
+        return true;
+    }
 }
